338fe0d26aa15310045f7a7855ae6b9e77b80384
Vazquinhos <vazquinhos@gmail.com>
2016-03-05 00:23:10 +0100

    Using io api to read a shader from a file

--- ogl/Engine/RenderSystem/io/CShader_io.cpp
+++ ogl/Engine/RenderSystem/io/CShader_io.cpp
@@ -1,71 +1,49 @@
 #include "io/io.h"
+#include "Logger/Logger.h"
 #include "Shader.h"
 #include "Types.h"
-#include "Logger\Logger.h"
+
 
 #include <flatbuffers/idl.h>
 #include <flatbuffers/util.h>
 
 #include "fbs/shader_generated.h" // Already includes "flatbuffers/flatbuffers.h".
 
-namespace iris { namespace io {
-
-	template <> bool Serialize(const char* filename, const char* schema, const CShader& aObject)
-	{
-		/*
-		// load FlatBuffer schema (.fbs) and JSON from disk
-		std::string schemafile;
-		std::string jsonfile;
-		bool ok = flatbuffers::LoadFile(filename , false, &schemafile) &&
-			flatbuffers::LoadFile(filename, false, &jsonfile);
-		if (!ok) {
-			printf("couldn't load files!\n");
-			return 1;
-		}
-
-		// parse schema first, so we can use it to parse the data after
-		flatbuffers::Parser parser;
-		const char *include_directories[] = { "../data/shaders/", nullptr };
-		ok = parser.Parse(schemafile.c_str(), include_directories) &&
-			parser.Parse(jsonfile.c_str(), include_directories);
-    printf("%s", parser.error_.c_str());
-
-	// load FlatBuffer schema (.fbs) and JSON from disk
-		std::string jsonfile;
-		bool ok = flatbuffers::LoadFile("../data/shaders/monsterdata.json", false, &jsonfile);
-		if (!ok) {
-			printf("couldn't load files!\n");
-			return 1;
-		}
-
-		// parse schema first, so we can use it to parse the data after
-		flatbuffers::Parser parser;
-		const char *include_directories[] = { "../data/shaders/", nullptr };
-		ok = parser.Parse(jsonfile.c_str(), include_directories);
-		printf("%s", parser.error_.c_str());
-
-		const MonsterManager* monster = GetMonsterManager(parser.builder_.GetBufferPointer());
-    auto vecoftables = monster->monsters();
-    for (auto it = vecoftables->begin(); it != vecoftables->end(); ++it)
+namespace iris
+{
+    namespace io
     {
-      printf("%d\n", it->hp());            // `80`
-      printf("%d\n", it->mana());          // default value of `150`
-      printf("%s\n", it->name()->c_str()); // "MyMonster"
-      printf("\n\n"); // "MyMonster"
+        template <> bool Load( const CResource& aResource, CShaderSPtr& aObject)
+        {
+            bool lOk = false;
+            uint8* lBufferPtr = nullptr;
+
+            if (GetBufferPtr(aResource, lBufferPtr))
+            {
+                const Shader* lShader = GetShader(lBufferPtr);
+
+                if (lShader)
+                {
+                    const char* lName = lShader->name()->c_str();
+                    CResource lShaderCode(lShader->filename()->c_str());
+                    aObject = CShaderSPtr(new CShader());
+
+                    if (aObject->Create(CShader::VERTEX_SHADER, lShaderCode.GetFileContent().c_str()))
+                    {
+                        lOk = true;
+                    }
+
+                }
+
+                free(lBufferPtr);
+            }
+
+            if( !lOk )
+            {
+                aObject = 0;
+            }
+
+            return lOk;
+        }
     }
-
-		// here, parser.builder_ contains a binary buffer that is the parsed data.
-        /*
-		// to ensure it is correct, we now generate text back from the binary,
-		// and compare the two:
-		std::string jsongen;
-		GenerateText(parser, parser.builder_.GetBufferPointer(), &jsongen);
-
-		if (jsongen != jsonfile) {
-			printf("%s----------------\n%s", jsongen.c_str(), jsonfile.c_str());
-		}*/
-
-		printf("The FlatBuffer has been parsed from JSON successfully.\n");
-		return true;
-	}
-}}
+}
