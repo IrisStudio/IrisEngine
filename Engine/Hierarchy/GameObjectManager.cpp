cca70c77aa504e6cc164d1381181b52fc863c016
Vazquinhos <vazquinhos@gmail.com>
2016-07-18 00:10:21 +0200

    corrections in fullscreen quad

--- Engine/Hierarchy/GameObjectManager.cpp
+++ Engine/Hierarchy/GameObjectManager.cpp
@@ -3,6 +3,7 @@
 #include "Components\DefaultComponents.h"
 #include "Components\DefaultSystems.h"
 #include "Window.h"
+#include "State.h"
 
 CGameObjectManager::CGameObjectManager()
 {
@@ -16,11 +17,11 @@ CGameObjectManager::CGameObjectManager()
     entityx::Entity entity = entities.create();
     entity.assign<Transform>();
     entity.assign<Velocity>(float3(1.0f, 0.0f, 0.0f), 1.0f);
-    entity.assign<Renderer>( "models/head/head.obj", eRP_Default );
+    //entity.assign<Renderer>( "models/head/head.obj", eRP_Default );
 
     //Create camera object
     entity = entities.create();
-    entity.assign<Transform>( float3(5.0f, 0.0f, 0.0f), float3(-1.0f, 0.0f, 0.0f) );
+    entity.assign<Transform>( float3(-0.0103685698f, 0.106419258f, 0.271069795f), float3(-0.0103685698f, -0.358434528f, -0.614286304f) );
     entity.assign<CCamera>();
     entity.assign<Controllable>();
     CCameraManager::Instance().SetCurrentCamera(entity.component<CCamera>().get());
@@ -33,5 +34,7 @@ void CGameObjectManager::update(entityx::TimeDelta dt)
 
     // Render
     systems.update<CameraSystem>(dt);
+
+    CState::EnableCullFace();
     systems.update<RenderSystem>(dt);
 }
